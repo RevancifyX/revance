ce87760f8cdfb35bb45e2a533d26452993a65aee
RevancifyX <140808499+RevancifyX@users.noreply.github.com>
2023-08-22 23:09:27 +0530

    Add files via upload

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if [ -z "$TERMUX_VERSION" ]; then
 fi
 
 if [ -d "$HOME/revance" ]; then
-    ./Revance/revancify
+    ./revance/revancify
     exit 0
 fi
 
@@ -31,7 +31,7 @@ Possible causes of error:
 fi
 
 if git clone --depth=1 https://github.com/RevancifyX/revance.git; then
-    $HOME/Revance/revancify
+    $HOME/revance/revancify
 else
     echo -e "\e[1;31mInstall Failed !!\e[0m"
     echo "Please Try again"
